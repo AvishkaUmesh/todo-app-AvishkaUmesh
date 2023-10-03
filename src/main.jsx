c5e6f6aa3d01b254e7780bdb2e74b1638e043be7
Avishka Umesh <umesh.avishka@gmail.com>
2023-10-03 20:25:57 +0530

    added Inter font

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,17 @@
+import { ThemeProvider } from '@mui/material';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import { TodoProvider } from './context/TodoContext';
 import './index.css';
+import theme from './theme.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
 		<TodoProvider>
-			<App />
+			<ThemeProvider theme={theme}>
+				<App />
+			</ThemeProvider>
 		</TodoProvider>
 	</React.StrictMode>
 );
